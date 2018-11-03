c73facb646d2c4a1c1d00416d0ea29f82d88b56e
Daniel Bou <43351001+DanielBou99@users.noreply.github.com>
2018-11-03 00:52:39 -0300

    Add files via upload

---
+++ Desafio040.py
@@ -0,0 +1,12 @@
+#Desafio 40
+
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+media = (n1+n2)/2
+
+if media < 5.0:
+    print('REPROVADO')
+elif media >= 5.0 and media <= 6.9:
+    print('RECUPERAÇÃO')
+else:
+    print('APROVADO')
