34e170ca6df5a27eff3942c3376c0a37994254ec
Daniel Bou <43351001+DanielBou99@users.noreply.github.com>
2018-11-09 00:13:16 -0200

    Update Dolar Cosco Shipping.py

--- Web Crawler/Dolar Cosco Shipping.py
+++ Web Crawler/Dolar Cosco Shipping.py
@@ -1,7 +1,12 @@
 import urllib.request
 import os
+from datetime import datetime
 
 while (True):
+
+    #Horario atual
+    horario = datetime.now()
+    horario= str(horario)
 
     # Importar o html do site
     content = urllib.request.urlopen("https://coscoshipping.com.br/pt/home/").read()
@@ -17,7 +22,8 @@ while (True):
     posicao = int(content.index(find)) + len(find)
     data = content[posicao : posicao + 10]
 
+    print('COSCO SHIPPING LINESdd')
     print('Data: {}'.format(data))
-    print('Dolar: {}'.format(dolar))
+    print('Dolar: {}\n'.format(dolar))
+    print('Ultima atualizacao do programa: {}'.format(horario[11:16]))
     os.system('pause')
-
